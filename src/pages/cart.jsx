3e690d9a7034a2852bd005a72cc69c6fee454ca5
Fadi Sarwat <fady.sarwat377@gmail.com>
2024-09-05 03:36:08 +0300

    Add empty state to cart page

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -41,6 +41,22 @@ export default function Cart() {
                             Items in your shopping cart
                         </h2>
 
+                        {cart.items.length === 0 && (
+                            <Link
+                                to="/"
+                                className="relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     stroke-width="1.5" stroke="currentColor" className="mx-auto h-12 w-12 text-gray-400">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"/>
+                                </svg>
+
+                                <span
+                                    className="mt-2 block text-sm font-semibold text-gray-900">No books in your cart, browse to find some.</span>
+                            </Link>
+                        )}
+
                         <ul role="list" className="divide-y divide-gray-200 border-b border-t border-gray-200">
                             {cart.items.map((item, itemIdx) => (
                                 <li key={item.id} className="flex py-6 sm:py-10">
