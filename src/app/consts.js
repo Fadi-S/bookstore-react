3fc44abfaf5409a20d7dae6ff2014495c0693e44
Fadi Sarwat <fadisarwat@yahoo.com>
2024-09-14 03:59:38 +0300

    Make server url a environement variable

--- src/app/consts.js
+++ src/app/consts.js
@@ -1,3 +1,3 @@
-export const ROOT_URL = "http://localhost:8080/api";
+export const ROOT_URL = process.env.REACT_APP_SERVER_URL;
 export const BOOK_IMAGE_URL = ROOT_URL + "/books/images/";
 export const USER_IMAGE_URL = ROOT_URL + "/profile/picture/";
